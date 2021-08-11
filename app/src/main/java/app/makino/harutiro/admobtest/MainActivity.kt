8799c17f4d4fcf988f6df66d23ce45d870991e91
harutiro <harutiro2027@outlook.jp>
2021-08-12 00:38:57 +0900

    add: 広告表示

--- app/src/main/java/app/makino/harutiro/admobtest/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/admobtest/MainActivity.kt
@@ -2,10 +2,24 @@ package app.makino.harutiro.admobtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+
+import com.google.android.gms.ads.AdView
+
+import com.google.android.gms.ads.MobileAds
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this)
+
+        val mAdView = findViewById<AdView>(R.id.adView)
+        val adRequest: AdRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
